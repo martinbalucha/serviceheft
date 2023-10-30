25afe50b55e4e19bd879da87c674b7b2e46f41d9
martinbalucha <martin.balucha@outlook.com>
2023-10-30 20:11:49 +0100

    Drafted the controller

---
+++ src/ServiceHeft/ServiceHeft.Webservice/CarController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHeft.Webservice;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CarController : ControllerBase
+{
+
+    [HttpPost]
+    public Task<IActionResult> CreateCar()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete]
+    public Task<IActionResult> DeleteCar()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut]
+    public Task<IActionResult> UpdateCar()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    public Task<IActionResult> GetCar() 
+    { 
+        throw new NotImplementedException(); 
+    }
+}
