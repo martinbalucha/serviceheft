ba913266773b28577aa572fed0aa33bd709fbd65
martinbalucha <martin.balucha@outlook.com>
2023-11-07 21:34:17 +0100

    Created generic EntityFrameworkRepository

---
+++ src/ServiceHeft.Persistence.EntityFramework/EntityFrameworkRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceHeft.Contracts;
+using ServiceHeft.Contracts.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHeft.Persistence.EntityFramework;
+
+public class EntityFrameworkRepository<TEntity> : IRepository<TEntity> where TEntity : Entity
+{
+    private readonly DbContext _dbContext;
+
+    public EntityFrameworkRepository(DbContext dbContext)
+    {
+        this._dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public Task<Guid> CreateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity?> FindAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TEntity>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
