94fc1e030d81039fb995c8136a9d538fffb3138e
martinbalucha <martin.balucha@outlook.com>
2023-11-07 21:26:21 +0100

    Fixed DbContext

--- src/ServiceHeft/ServiceHeft.Server/Persistence/ServiceHeftDbContext.cs
+++ src/ServiceHeft/ServiceHeft.Server/Persistence/ServiceHeftDbContext.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ServiceHeft.Contracts.Servicing.Automotive;
+using ServiceHeft.Contracts.Servicing.Maintenance;
 
 namespace ServiceHeft.Server.Persistence;
 
 public class ServiceHeftDbContext : DbContext
 {
+    public DbSet<ServiceRecord> ServiceRecords { get; set; }
+    public DbSet<Car> Cars { get; set; }
+
+    public ServiceHeftDbContext(DbContextOptions<ServiceHeftDbContext> options) : base(options)
+    {    
+    }
 }
