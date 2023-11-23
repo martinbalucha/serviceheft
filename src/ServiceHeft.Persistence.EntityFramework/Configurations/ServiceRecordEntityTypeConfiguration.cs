e8c685c2388810277ec04512cf7b3ea3e89b3252
martinbalucha <martin.balucha@outlook.com>
2023-11-23 21:10:43 +0100

    Service record entity configuration done

---
+++ src/ServiceHeft.Persistence.EntityFramework/Configurations/ServiceRecordEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceHeft.Maintenance.Contracts.Servicing.Maintenance;
+
+namespace ServiceHeft.Persistence.EntityFramework.Configurations;
+
+public class ServiceRecordEntityTypeConfiguration : IEntityTypeConfiguration<ServiceRecord>
+{
+    private const int NameMaxLength = 256;
+    private const int DescriptionMaxLength = 1000;
+
+    public void Configure(EntityTypeBuilder<ServiceRecord> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(NameMaxLength);
+        builder.Property(x => x.Description).HasMaxLength(DescriptionMaxLength);
+        
+        builder.HasMany(x => x.PartsChanged);
+    }
+}
