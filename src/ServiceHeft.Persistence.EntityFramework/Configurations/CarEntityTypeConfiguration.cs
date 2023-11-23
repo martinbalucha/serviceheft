1970ef946f476d46f75a191c18ed1932c6b2aa15
martinbalucha <martin.balucha@outlook.com>
2023-11-23 21:08:00 +0100

    Removed usings

--- src/ServiceHeft.Persistence.EntityFramework/Configurations/CarEntityTypeConfiguration.cs
+++ src/ServiceHeft.Persistence.EntityFramework/Configurations/CarEntityTypeConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ServiceHeft.Maintenance.Contracts.Servicing.Automotive;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServiceHeft.Persistence.EntityFramework.Configurations;
 
