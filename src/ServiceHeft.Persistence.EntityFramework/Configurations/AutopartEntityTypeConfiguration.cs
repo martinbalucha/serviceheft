6327faa90d14983d8be7a9287282d3708b7b9aec
martinbalucha <martin.balucha@outlook.com>
2023-11-23 21:22:28 +0100

    Added autopart configuration

---
+++ src/ServiceHeft.Persistence.EntityFramework/Configurations/AutopartEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceHeft.Maintenance.Contracts.Servicing.Maintenance;
+
+namespace ServiceHeft.Persistence.EntityFramework.Configurations;
+
+public class AutopartEntityTypeConfiguration : IEntityTypeConfiguration<Autopart>
+{
+    private const int NameMaxLength = 60;
+    private const int ProducerMaxLength = 60;
+
+    public void Configure(EntityTypeBuilder<Autopart> builder)
+    {
+        builder.HasKey(a => a.Id);
+        builder.Property(a => a.Name).HasMaxLength(NameMaxLength);
+        builder.Property(a => a.Producer).HasMaxLength(ProducerMaxLength);
+    }
+}
