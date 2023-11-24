9af357e0e61d0265e3a402259d6e0d758b1f3f4e
martinbalucha <martin.balucha@outlook.com>
2023-11-24 20:05:37 +0100

    Added mock service in domain

---
+++ src/ServiceHeft/ServiceHeft.Maintenance.Contracts/Servicing/Maintenance/IMaintenanceAggregateService.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceHeft.Maintenance.Contracts.Servicing.Maintenance;
+
+/// <summary>
+/// Provides aggregation of maintenance performed on <see cref="Car"/>
+/// </summary>
+public interface IMaintenanceAggregateService
+{
+    Task GetCarServiceAggregate(Guid carId);
+}
---
+++ src/ServiceHeft/ServiceHeft.Maintenance.Domain/ServiceAggregatingService.cs
@@ -0,0 +1,11 @@
+﻿using ServiceHeft.Maintenance.Contracts.Servicing.Maintenance;
+
+namespace ServiceHeft.Maintenance.Domain;
+
+public class ServiceAggregatingService : IMaintenanceAggregateService
+{
+    public Task GetCarServiceAggregate(Guid carId)
+    {
+        throw new NotImplementedException();
+    }
+}
