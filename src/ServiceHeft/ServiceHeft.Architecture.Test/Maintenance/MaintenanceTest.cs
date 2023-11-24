aef05c722af8d67643ef4eeee24bebeb1f33ef4e
martinbalucha <martin.balucha@outlook.com>
2023-11-24 19:44:58 +0100

    Created first architecture test

---
+++ src/ServiceHeft/ServiceHeft.Architecture.Test/Maintenance/MaintenanceTest.cs
@@ -0,0 +1,35 @@
+﻿using NetArchTest.Rules;
+using ServiceHeft.Maintenance.Contracts;
+using Xunit;
+
+namespace ServiceHeft.Architecture.Test.Contracts;
+
+public class MaintenanceTest
+{
+    private const string ContractsNamespace = "ServiceHeft.Maintenance.Contracts";
+    private const string DomainNamespace = "ServiceHeft.Maintenance.Domain";
+    private const string EntityFrameworkNamespace = "ServiceHeft.Persistence.EntityFramework";
+    private const string WebserviceNamespace = "ServiceHeft.Maintenance.Webservice";
+
+    [Fact]
+    public void Contracts_ShouldNot_DependOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Entity).Assembly;
+
+        var otherDependencies = new[]
+        {
+            DomainNamespace,
+            EntityFrameworkNamespace,
+            WebserviceNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAny(otherDependencies).GetResult();
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+    }
+
+    
+}
