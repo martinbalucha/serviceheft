f66517660546096d85b30e420ec8629f9c8a74e4
martinbalucha <38705514+martinbalucha@users.noreply.github.com>
2023-11-26 21:59:08 +0100

    11 configure serilog logging (#23)
    * Added serilog configuration
    
    * Added instance injection

--- src/ServiceHeft/ServiceHeft.Maintenance.Domain/MaintenanceAggregatingService.cs
+++ src/ServiceHeft/ServiceHeft.Maintenance.Domain/MaintenanceAggregatingService.cs
@@ -1,9 +1,17 @@
-﻿using ServiceHeft.Maintenance.Contracts.Maintenance;
+﻿using Serilog;
+using ServiceHeft.Maintenance.Contracts.Maintenance;
 
 namespace ServiceHeft.Maintenance.Domain;
 
 public class MaintenanceAggregatingService : IMaintenanceAggregateService
 {
+    private readonly ILogger logger;
+
+    public MaintenanceAggregatingService(ILogger logger)
+    {
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
     public Task GetCarServiceAggregate(Guid carId)
     {
         throw new NotImplementedException();
