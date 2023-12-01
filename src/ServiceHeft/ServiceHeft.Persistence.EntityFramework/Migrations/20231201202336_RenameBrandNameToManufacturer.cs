6b3dc0700ca241fa91eddb9feabc97ab59147e4c
martinbalucha <martin.balucha@outlook.com>
2023-12-01 21:24:36 +0100

    Created migration

---
+++ src/ServiceHeft/ServiceHeft.Persistence.EntityFramework/Migrations/20231201202336_RenameBrandNameToManufacturer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceHeft.Persistence.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameBrandNameToManufacturer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModelInfo_Brand",
+                table: "Cars",
+                newName: "ModelInfo_Manufacturer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModelInfo_Manufacturer",
+                table: "Cars",
+                newName: "ModelInfo_Brand");
+        }
+    }
+}
