ff2f9d907ee6a2597467ca3eca0fc86c7042e72b
martinbalucha <martin.balucha@outlook.com>
2023-12-04 09:17:32 +0100

    Created car service

---
+++ src/ServiceHeft/ServiceHeft.Maintenance.Contracts/Automotive/ICarService.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceHeft.Maintenance.Contracts.Automotive;
+
+public interface ICarService
+{
+    Task CreateAsync(Car car);
+    Task UpdateAsync(Car car);
+    Task DecommissionAsnyc(Guid car);
+    Task DeleteAsync(Car car);
+    Task<Car> FindAsync(Guid guid);
+}
---
+++ src/ServiceHeft/ServiceHeft.Maintenance.Domain/Automotive/CarService.cs
@@ -0,0 +1,36 @@
+﻿using ServiceHeft.Maintenance.Contracts.Automotive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHeft.Maintenance.Domain.Automotive;
+
+public class CarService : ICarService
+{
+    public Task CreateAsync(Car car)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DecommissionAsnyc(Guid car)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Car car)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Car> FindAsync(Guid guid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Car car)
+    {
+        throw new NotImplementedException();
+    }
+}
