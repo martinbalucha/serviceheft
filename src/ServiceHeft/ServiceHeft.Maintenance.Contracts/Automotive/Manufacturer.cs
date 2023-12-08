0240634bf654c74453f5d94c0e0a2d39ca9c5f22
martinbalucha <martin.balucha@outlook.com>
2023-12-08 21:58:59 +0100

    Adjusted manufacturer
    Created manufacturer

---
+++ src/ServiceHeft/ServiceHeft.Maintenance.Contracts/Automotive/Manufacturer.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceHeft.Maintenance.Contracts.Automotive;
+
+public class Manufacturer
+{
+    public string Name { get; private set; }
+
+    public Manufacturer(string name)
+    {
+        Name = name;
+    }
+}
