bde75049bdfb71c7cde12f53a440f123f2ef1f00
martinbalucha <martin.balucha@outlook.com>
2023-12-10 13:25:08 +0100

    Created entity model for Model

---
+++ src/ServiceHeft/ServiceHeft.Persistence.EntityFramework/Configurations/ModelEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceHeft.Maintenance.Contracts.Automotive;
+
+namespace ServiceHeft.Persistence.EntityFramework.Configurations;
+
+public class ModelEntityTypeConfiguration : IEntityTypeConfiguration<Model>
+{
+    private const int ModelNameMaxLength = 40;
+
+    public void Configure(EntityTypeBuilder<Model> builder)
+    {
+        builder.HasKey(m => new { m.Manufacturer, m.InternalName });
+        builder.Property(m => m.InternalName).HasMaxLength(ModelNameMaxLength);
+        builder.Property(m => m.OfficialName).HasMaxLength(ModelNameMaxLength);
+    }
+}
