93c623f1d98b249b090f13bc105d5adbb2638d07
martinbalucha <martin.balucha@outlook.com>
2023-12-13 20:39:06 +0100

    Created build configuration

---
+++ src/ServiceHeft/ServiceHeft.Maintenance.Infrastructure.Test/Utils/TestConfigurationBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+
+namespace ServiceHeft.Maintenance.Infrastructure.Test.Utils;
+
+public static class TestConfigurationBuilder
+{
+    public static IConfigurationRoot Build(string jsonConfiguration)
+    {
+        return new ConfigurationBuilder()
+            .AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(jsonConfiguration)))
+            .Build();
+    }
+
+    public static IConfigurationRoot Build(object configuration)
+    {
+        string jsonConfiguration = JsonSerializer.Serialize(configuration);
+        return Build(jsonConfiguration);
+    }
+}
