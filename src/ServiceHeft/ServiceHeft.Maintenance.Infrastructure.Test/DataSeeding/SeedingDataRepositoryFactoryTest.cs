54afdc4ed3eb83922acf514012b444cf15e03724
martinbalucha <martin.balucha@outlook.com>
2023-12-13 20:43:18 +0100

    Wrote first factory test

---
+++ src/ServiceHeft/ServiceHeft.Maintenance.Infrastructure.Test/DataSeeding/SeedingDataRepositoryFactoryTest.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using ServiceHeft.Maintenance.Contracts.Automotive;
+using ServiceHeft.Maintenance.Infrastructure.Test.Utils;
+using ServiceHeft.Persistence.EntityFramework.DataSeeding;
+using System.IO.Abstractions;
+using Xunit;
+
+namespace ServiceHeft.Maintenance.Infrastructure.Test.DataSeeding;
+
+public class SeedingDataRepositoryFactoryTest
+{
+    private readonly Mock<IFile> file = new();
+
+    [Fact]
+    public void Create_ValidConfiguration_RepositoryCreated()
+    {
+        // Arrange
+        var appSettings = new
+        {
+            DataSeeding = new
+            {
+                Model = new
+                {
+                    DataSeedingConfiguration = new
+                    {
+                        SeedingFilePath = "seedingFile"
+                    }
+                }
+            }
+        };
+
+        var configuration = TestConfigurationBuilder.Build(appSettings);
+
+        var seedingDataRepositoryFactory = new SeedingDataRepositoryFactory(file.Object, configuration);
+
+        // Act
+        var repository = seedingDataRepositoryFactory.Create<Model>();
+
+        // Assert
+        Assert.IsType<SeedingDataRepository<Model>>(repository);
+    }
+}
